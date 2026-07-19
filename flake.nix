4df4ed6cdb3ddae02cd87a6f92925b4ffa3d7ad9
steve Edwards <edwardsm_99@yahoo.com>
2026-07-19 08:08:44 -0400

    Made flake point to home directory places and configuration now points to ~/hosts/desktop/

--- flake.nix
+++ flake.nix
@@ -1,34 +1,60 @@
 {
-  description = "A simple NixOS flake";
+  description = "Steven's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     steamtinkerlaunch = {
       url = "github:sonic2kk/steamtinkerlaunch/master";
       flake = false;
     };
-    
+
     heroic = {
       url = "github:Heroic-Games-Launcher/HeroicGamesLauncher/main";
       flake = false;
     };
 
     hytale-launcher.url = "github:JPyke3/hytale-launcher-nix";
-  }
+  };
 
-  outputs = { self, home-manager, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
 
       modules = [
         ./hosts/desktop/configuration.nix
-        # Add more modules here as needed
+
+        # Overlay for unstable packages
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              unstable = import inputs.nixpkgs-unstable {
+                inherit (prev) system;
+                config.allowUnfree = true;
+              };
+            })
+          ];
+        }
+
         home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs; };
+
+            users.steve = import ./home.nix;
+          };
+        }
       ];
     };
-  }
+  };
 }
--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -3,16 +3,6 @@
 
 
 {
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    extraSpecialArgs = { inherit inputs; };
-    users.steve = import ./home.nix;
-    backupFileExtension = "backup";
-  };
-}
-
-{
   imports = [
     ../../hardware-configuration.nix
     ./hardware.nix
@@ -22,25 +12,11 @@
     ./virtualization.nix
     ./filesystems.nix
     ./audio-recording.nix
-#    ./edxd.nix
-#    ./home.nix
   ];
 
   # Basic system settings
-  #system.stateVersion = "25.11";
+  system.stateVersion = "25.11";
   time.timeZone = "America/New_York";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
     
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -48,5 +24,16 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
   nix.settings.download-buffer-size = 734003200; # 700 MB
+
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    users.steve = import ./hosts/desktop/home.nix;
+    backupFileExtension = "backup";
+  };
+}
+
 
 }
